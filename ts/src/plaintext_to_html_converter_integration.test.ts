4eb7173b7c99ce1d860364e6cea4ed7d25400bc8
saleheenshafiq9 <48786700+saleheenshafiq9@users.noreply.github.com>
2022-02-17 11:46:55 +0600

    Delete ts directory

--- ts/src/plaintext_to_html_converter_integration.test.ts
+++
@@ -1,8 +0,0 @@
-import { PlaintextToHtmlConverter } from './plaintext_to_html_converter'
-
-describe("Text Converter integration test", () => {
-  it("sample.txt", () => {
-    const converter = new PlaintextToHtmlConverter()
-    expect(converter.toHtml()).toEqual("abc<br />&lt;hello&gt;")
-  })
-})
